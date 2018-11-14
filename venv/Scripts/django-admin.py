a7b2118a9f2ab3eec346b999f0c88f03d554dceb
Avgustin <avgustinalex@yandex.ru>
2018-11-14 14:41:05 +0300

    esketitititittiti

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\avgus\onedrive\desktop\проекты\esketit\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
